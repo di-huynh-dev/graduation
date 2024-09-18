83d093eac752c3a75cb62b6fbf9be849d7486839
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2024-09-18 23:11:53 +0800

    #1 add urlnamatamu and add scrolling animation 100%

---
+++ js/urlNamaTamu.js
@@ -0,0 +1,8 @@
+const urlParams = new URLSearchParams(window.location.search);
+const nama = urlParams.get("n") || "";
+const pronoun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
+const namaContainer = document.querySelector(".open-title span");
+
+namaContainer.innerText = `${pronoun} ${nama}`.replace(/ ,$/, ",");
+
+document.getElementById("fullname").value = nama;
