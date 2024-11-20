e0f759954c16b3f38b701034a9e7d8ac345f6294
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2024-11-20 20:20:55 +0800

    update waktu akad dan resepsi

--- js/countdown-controller.js
+++ js/countdown-controller.js
@@ -2,7 +2,7 @@ simplyCountdown(".simply-countdown", {
 	year: 2024, // required
 	month: 12, // required
 	day: 7, // required
-	hours: 8, // Default is 0 [0-23] integer
+	hours: 7, // Default is 0 [0-23] integer
 	words: {
 		//words displayed into the countdown
 		days: { singular: "hari", plural: "hari" },
