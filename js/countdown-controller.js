876c857c02d9a003f7b3860e24db6d6bca68eade
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-12-10 23:30:41 +0700

    commit

--- js/countdown-controller.js
+++ js/countdown-controller.js
@@ -1,13 +1,14 @@
 simplyCountdown(".simply-countdown", {
-	year: 2024, // required
-	month: 12, // required
-	day: 7, // required
-	hours: 7, // Default is 0 [0-23] integer
-	words: {
-		//words displayed into the countdown
-		days: { singular: "hari", plural: "hari" },
-		hours: { singular: "jam", plural: "jam" },
-		minutes: { singular: "menit", plural: "menit" },
-		seconds: { singular: "detik", plural: "detik" },
-	},
+  year: 2024, // required
+  month: 12, // required
+  day: 19, // required
+  hours: 10, // Default is 0 [0-23] integer, set to 0 for UTC+7 adjustment
+  words: {
+    //words displayed into the countdown
+    days: { singular: "ngày", plural: "ngày" },
+    hours: { singular: "giờ", plural: "giờ" },
+    minutes: { singular: "phút", plural: "phút" },
+    seconds: { singular: "giây", plural: "giây" },
+  },
+  timezone: 7, // Add this line to set the timezone to UTC+7
 });
